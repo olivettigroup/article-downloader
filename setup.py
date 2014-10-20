c4b585cd2d73882480276fc75bd77efef9064197
Edward Kim <eddotman@gmail.com>
2014-10-20 13:13:02 -0400

    finished pypi setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+name = 'article-downloader',
+packages = ['article-downloader'], # this must be the same as the name above
+version = '0.1',
+description = 'A class for downloading scientific journal articles',
+author = 'Edward Kim',
+author_email = 'eddotman@gmail.com',
+url = 'https://github.com/eddotman/article-downloader', # use the URL to the github repo
+download_url = 'https://www.github.com/eddotman/article-downloader/tarball/0.1', # I'll explain this in a second
+keywords = ['journal', 'paper', 'article', 'downloader'], # arbitrary keywords
+)
