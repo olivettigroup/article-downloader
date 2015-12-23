86402185f785b16e18be6398f37de1bb1b3ebe69
Edward Kim <eddotman@gmail.com>
2015-12-23 12:11:10 -0500

    added try block

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 setup(
 name = 'articledownloader',
 packages = ['articledownloader'], # this must be the same as the name above
-version = '3.1',
+version = '3.2',
 description = 'A class for downloading scientific journal articles',
 author = 'Edward Kim',
 author_email = 'eddotman@gmail.com',
 url = 'https://github.com/eddotman/article-downloader', # use the URL to the github repo
-download_url = 'https://www.github.com/eddotman/article-downloader/tarball/3.1',
+download_url = 'https://www.github.com/eddotman/article-downloader/tarball/3.2',
 keywords = ['journal', 'paper', 'article', 'downloader'], # arbitrary keywords
 )
