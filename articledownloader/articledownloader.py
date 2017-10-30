a55aec6ae18e79a93a1060d84b471bc0468c3696
Zach Jensen <zjensen@mit.edu>
2017-10-30 16:24:07 -0400

    update aaas

--- articledownloader/articledownloader.py
+++ articledownloader/articledownloader.py
@@ -308,7 +308,7 @@ class ArticleDownloader:
       article_url = 'http://dx.doi.org/' + doi
       resp = requests.get(article_url, headers=headers, timeout=self.timeout_sec)
 
-      download_url = resp.url + doi + '.full'  #Capture fulltext from redirect
+      download_url = resp.url + '.full'  #Capture fulltext from redirect
 
       r = requests.get(download_url, stream=True, headers=headers, timeout=self.timeout_sec)
       if r.status_code == 200:
