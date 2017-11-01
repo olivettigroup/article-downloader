f41685ed5462c9b5de99fca6ee6066cc5477e82e
Edward Kim <eddotman@gmail.com>
2017-11-01 15:01:18 -0400

    PyPI release updates

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup
 setup(
 name = 'articledownloader',
 packages = ['articledownloader'], # this must be the same as the name above
-version = '6.8',
-description = 'A class for downloading scientific journal articles',
-author = 'Edward Kim',
-author_email = 'eddotman@gmail.com',
+version = '7.0',
+description = 'A module for downloading scientific journal articles',
+author = 'The Synthesis Project',
+author_email = 'synthesisproject@mit.edu',
 url = 'https://github.com/olivettigroup/article-downloader', # use the URL to the github repo
 download_url = 'https://www.github.com/olivettigroup/article-downloader/tarball/6.8',
 keywords = ['journal', 'paper', 'article', 'downloader'], # arbitrary keywords
