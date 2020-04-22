710f8442c12b1363c5daea9ead29c82467e390cf
Edward Kim <ekim@citrine.io>
2020-04-22 11:58:57 -0600

    push dists

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 setup(
 name = 'articledownloader',
 packages = ['articledownloader'], # this must be the same as the name above
-version = '9.1',
+version = '9.1.1',
 description = 'A module for downloading scientific journal articles',
 author = 'The Synthesis Project',
 author_email = 'synthesisproject@mit.edu',
 url = 'https://github.com/olivettigroup/article-downloader', # use the URL to the github repo
-download_url = 'https://www.github.com/olivettigroup/article-downloader/tarball/8.5',
+download_url = 'https://www.github.com/olivettigroup/article-downloader/tarball/9.1.1',
 keywords = ['journal', 'paper', 'article', 'downloader'], # arbitrary keywords
 )
